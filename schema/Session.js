900d04a8de024ed17022efa07639fe6e1dede9c0
Aman Dalmia <amandalmia18@gmail.com>
2021-05-10 14:19:04 +0530

    NEW: added new cube to session schema

--- schema/Session.js
+++ schema/Session.js
@@ -64,3 +64,45 @@ cube(`Session`, {
 
   dataSource: `default`,
 });
+
+cube(`GroupedSession`, {
+  sql: `
+    WITH summary AS (
+    SELECT session.plio_id,
+         session.user_id,
+         session.watch_time,
+         ROW_NUMBER() OVER(PARTITION BY session.user_id, session.plio_id
+                           ORDER BY session.watch_time DESC) AS id
+         FROM "public"."session"
+     )
+    SELECT *
+        FROM summary
+    WHERE id = 1
+    `,
+
+  joins: {
+    Plio: {
+      sql: `${CUBE}.plio_id = ${Plio}.id`,
+      relationship: `belongsTo`,
+    },
+
+    User: {
+      sql: `${CUBE}.user_id = ${User}.id`,
+      relationship: `belongsTo`,
+    },
+  },
+
+  measures: {
+    averageWatchTime: {
+      sql: `watch_time`,
+      type: `avg`,
+    },
+  },
+  dimensions: {
+    id: {
+      sql: `id`,
+      type: `number`,
+      primaryKey: true,
+    },
+  },
+});
