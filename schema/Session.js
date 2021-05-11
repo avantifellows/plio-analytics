eb6d6532959a8039b5ad075d2a45869d2044be6c
Vaibhav Rathore <vaibhav@coloredcow.com>
2021-05-11 13:50:43 +0530

    self review. schema files are back in

---
+++ schema/Session.js
@@ -0,0 +1,66 @@
+cube(`Session`, {
+  sql: `SELECT * FROM public.session`,
+
+  joins: {
+    Plio: {
+      sql: `${CUBE}.plio_id = ${Plio}.id`,
+      relationship: `belongsTo`,
+    },
+
+    User: {
+      sql: `${CUBE}.user_id = ${User}.id`,
+      relationship: `belongsTo`,
+    },
+  },
+
+  measures: {
+    count: {
+      type: `count`,
+      drillMembers: [id, hasVideoPlayed, createdAt, updatedAt],
+    },
+    uniqueUsers: {
+      sql: `${userId}`,
+      type: `countDistinct`,
+    },
+  },
+
+  dimensions: {
+    id: {
+      sql: `id`,
+      type: `number`,
+      primaryKey: true,
+    },
+
+    userId: {
+      sql: `${User}.id`,
+      type: `number`,
+    },
+
+    retention: {
+      sql: `retention`,
+      type: `string`,
+    },
+
+    hasVideoPlayed: {
+      sql: `has_video_played`,
+      type: `string`,
+    },
+
+    createdAt: {
+      sql: `created_at`,
+      type: `time`,
+    },
+
+    updatedAt: {
+      sql: `updated_at`,
+      type: `time`,
+    },
+
+    deleted: {
+      sql: `deleted`,
+      type: `time`,
+    },
+  },
+
+  dataSource: `default`,
+});
